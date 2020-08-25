1a3b9058f3c1d77b76357ce33dc1a49d81530503
mseweryn45 <mseweryn45@users.noreply.github.com>
2020-08-25 10:04:22 +0200

    Add elements

---
+++ dist/script.js
@@ -0,0 +1,19 @@
+// jQuery(function ($) {
+//     setInterval(function () {
+//         // $('.col-right-img').removeClass("col-right-img-prev").addClass("col-right-img-next");
+//         // $('.col-right-img').removeClass("col-right-img-active").addClass("col-right-img-prev");
+//         // $('.col-right-img').removeClass("col-right-img-next").addClass("col-right-img-activ");
+        
+//         console.log("tak");
+//         if ($('.col-right-img').hasClass('col-right-img-prev') == true){
+//             $('.col-right-img-prev').removeClass("col-right-img-prev").addClass("col-right-img-next");
+//         };
+//         if ($('.col-right-img').hasClass('col-right-img-activ') == true){
+//             $('.col-right-img-prev').removeClass("col-right-img-prev").addClass("col-right-img-next");
+//         };
+//         if ($('.col-right-img').hasClass('col-right-img-next') == true){
+//             $('.col-right-img-prev').removeClass("col-right-img-prev").addClass("col-right-img-next");
+//         }
+
+//     }, 3000);
+// });
